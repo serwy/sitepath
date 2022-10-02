61efad0098b6fae6ead71d8d93d280fef2f78315
Roger D. Serwy <roger.serwy@gmail.com>
2022-10-01 21:37:31 -0500

    repository init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='sitepath',
+    version='0.0.0',
+    author='Roger D. Serwy',
+    author_email='roger.serwy@gmail.com',
+    url="http://github.com/serwy/sitepath",
+    packages=['sitepath'],
+    description='sitepath',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
